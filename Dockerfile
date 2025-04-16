93481d8a475641aa9e8a8f620b2ab1be8e555b5e
Ginu5952 <ginujestin@gmail.com>
2025-04-16 19:54:03 +0200

    Modified Docker file

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,20 @@
-
-FROM node:18-alpine as builder
+# Base image
+FROM node:18
 
 
 WORKDIR /app
 
 
-COPY package*.json /app/
+COPY package.json package-lock.json ./
 
 
 RUN npm install
 
 
-COPY . /app/
+COPY . .
 
 
-RUN npm run build
+EXPOSE 5173
 
 
-FROM nginx:alpine
-
-
-COPY --from=builder /app/dist /usr/share/nginx/html
-
-EXPOSE 80
-
-
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["npm", "run", "dev", "--", "--host"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer/',
+    }
+  }
+});
